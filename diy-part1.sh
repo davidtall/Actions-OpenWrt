8452dfd4fabcbaa23846cab749f0cd6e2d696ab1
david <269700927@qq.com>
2023-04-18 18:25:16 +0800

    360t7

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 function git_clone() {
   git clone --depth 1 $1 $2 || true
@@ -35,31 +18,17 @@ mv -n `find $1/* -maxdepth 0 -type d` ./
 rm -rf $1
 }
 
-rm -rf package/custom
-mkdir package/custom
-cd package/custom
+git clone https://github.com/kenzok8/small-package package/small-package
+pushd package/small-package > /dev/null
 
-git clone --depth 1 https://github.com/kiddin9/luci-theme-edge
-git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon.git
+mkdir tmp
+mv luci-app-{passwall*,onliner,beardropper,iptvhelper,tcpdump,netdata,netspeedtest,supervisord,smartdns,ssr-plus,mosdns,adguardhome,dockerman,wrtbwmon,pushbot,argon-config,design-config} tmp/
+rm -rf {luci-app-*,luci-theme-*}
+mv tmp/* ./ && rm -rf tmp
+git clone --depth 1 -b 18.06 https://github.com/kiddin9/luci-theme-edge
+git clone --depth 1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+git clone https://github.com/gngpp/luci-theme-design.git
 rm -rf luci-theme-edge/root/etc/uci-defaults/30_luci-theme-edge
 
-
-git clone --depth 1 https://github.com/kenzok8/wall && mv -n wall/* ./ ; rm -rf alist && rm -rf wall
-git clone --depth 1 https://github.com/kiddin9/aria2
-git clone --depth 1 https://github.com/ElvenP/luci-app-onliner
-git clone --depth 1 https://github.com/riverscn/openwrt-iptvhelper && mvdir openwrt-iptvhelper
-git clone --depth 1 https://github.com/NateLol/luci-app-beardropper
-git clone --depth 1 https://github.com/KFERMercer/luci-app-tcpdump
-git clone -b 1.33.1 https://github.com/Jason6111/luci-app-netdata
-git clone --depth 1 https://github.com/sundaqiang/openwrt-packages && mv -n openwrt-packages/luci-app-supervisord ./; rm -rf openwrt-packages
-git clone --depth 1 -b lede https://github.com/pymumu/luci-app-smartdns
-git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2 passwall2 && mv -n passwall2/luci-app-passwall2 ./;rm -rf passwall2
-#git clone --depth 1 -b luci https://github.com/xiaorouji/openwrt-passwall passwall && mv -n passwall/luci-app-passwall ./;rm -rf passwall
-#git clone --depth 1 https://github.com/fw876/helloworld && mv -n helloworld/luci-app-ssr-plus helloworld/naiveproxy ./ ; rm -rf helloworld
-git clone --depth 1 https://github.com/QiuSimons/openwrt-mos && mv -n openwrt-mos/luci-app-mosdns ./ ; rm -rf openwrt-mos
-git clone --depth 1 https://github.com/sirpdboy/sirpdboy-package && mv -n sirpdboy-package/{luci-app-adguardhome,luci-app-dockerman} ./ ; rm -rf sirpdboy-package 
-git clone --depth 1 https://github.com/brvphoenix/luci-app-wrtbwmon wrtbwmon1 && mvdir wrtbwmon1
-git clone --depth 1 https://github.com/brvphoenix/wrtbwmon wrtbwmon2 && mvdir wrtbwmon2
-
+#rm -rf upx*
 sed -i 's/+mosdns-v5/+mosdns/g' luci-app-mosdns/Makefile
-rm -rf upx*
--- diy-part2.sh
+++ diy-part2.sh
@@ -13,7 +13,13 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-cd feeds/luci/themes
-mv luci-theme-bootstrap luci-bak-bootstrap
-rm -rf luci-theme-*
-mv luci-bak-bootstrap luci-theme-bootstrap
+mv feeds/luci/themes/luci-theme-bootstrap feeds/luci/themes/luci-bak-bootstrap
+rm -rf feeds/luci/themes/luci-theme-*
+mv feeds/luci/themes/luci-bak-bootstrap feeds/luci/themes/luci-theme-bootstrap
+
+rm -rf feeds/packages/lang/golang
+svn export https://github.com/coolsnowwolf/packages/trunk/lang/golang feeds/packages/lang/golang
+
+rm -rf feeds/luci/applications/luci-app-{passwall,mosdns,dockerman}
+rm -rf feeds/packages/net/{shadowsocks*,xray*,v2ray*,mosdns,aria*}
+cp -Rp package/small-package/{aria*,shadowsocks*,xray*,v2ray*,mosdns} feeds/packages/net/
